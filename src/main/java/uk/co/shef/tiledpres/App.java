10bc394e050b93565994719b50e1860b72f58881
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2009-08-27 17:17:03 +0100

    First pass

---
+++ src/main/java/uk/co/shef/tiledpres/App.java
@@ -0,0 +1,13 @@
+package uk.co.shef.tiledpres;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
